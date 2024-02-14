31a99ac22c43fdef6eca7659b4f78095050eff1c
RenanCDev <renan.costa.bsi@gmail.com>
2024-02-13 23:21:50 -0300

    test: Implementing unittests with pytest

--- src/validators/tag_creator_validator.py
+++ src/validators/tag_creator_validator.py
@@ -1,17 +1,11 @@
-from cerberus import Validator as vl
-from src.errors.error_types.http_unprocessable_entity import HttpUnprocessableEntityError as uee
-
-'''
-{
-    "product_code": "123"
-}
-'''
+from cerberus import Validator
+from src.errors.error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 def tag_creator_validator(request: any) -> None:
-    body_validator = vl({
+    body_validator = Validator({
         "product_code": { "type": "string", "required": True, "empty": False }
     })
 
     response = body_validator.validate(request.json)
     if response is False:
-        raise uee(body_validator.errors)
+        raise HttpUnprocessableEntityError(body_validator.errors)
